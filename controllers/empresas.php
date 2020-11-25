d4c7931cbd6b210afb18a32e60a20c41da6a4799
Victor Hugo Hernandez <victortoy29@gmail.com>
2020-11-24 19:00:46 -0500

    Update empresas

--- controllers/empresas.php
+++ controllers/empresas.php
@@ -1,43 +1,10 @@
 <?php
 require_once '../libs/model.php';
 
-class empresas{
-    public function insert($datos){
-        $sql = "INSERT INTO empresas SET ";
-        foreach($datos as $key => $value){
-            $sql .= "$key = '$value',";
-        }
-        $sql = rtrim($sql,',');
-        $db = new database();
-        return $db->consulta($sql);
-    }
+class empresas extends model{
+    protected $tabla = 'empresas';
 
-    public function select($datos){
-        $sql = "SELECT * 
-                FROM empresas
-                WHERE 1 ";
-        foreach ($datos as $key => $value) {
-            $sql .= "AND $key = '$value'";
-        }
-        $db = new database();
-        return $db->consulta($sql);
-    }
-
-    public function update($datos){
-        $sql = "UPDATE empresas SET ";
-        foreach ($datos as $key => $value) {
-            $sql .= "$key = '$value',";
-        }
-        $sql = rtrim($sql,',');
-        $sql .= " WHERE id = ".$datos['id'];
-        $db = new database();
-        return $db->consulta($sql);
-    }
-
-    public function delete($datos){
-        $sql = "DELETE FROM empresas WHERE id = ".$datos['id'];
-        $db = new database();
-        return $db->consulta($sql);   
+    public function prueba($datos){
     	$sql = "SELECT COUNT(1) AS cantidad FROM empresas";
     	$db = new database();
     	return $db->consulta($sql);
